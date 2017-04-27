cf80ace9a13316525efc55c2d5c501369da7b4b2
Nquere <nquere.8@gmail.com>
2017-04-27 13:59:21 +0100

    Mise en commun

--- src/AppBundle/Controller/SkOController.php
+++ src/AppBundle/Controller/SkOController.php
@@ -21,64 +21,71 @@ class SkOController extends Controller
     public function indexAction(Request $request)
     {
         $player = new Player;
-        
+
         $form = $this->createFormBuilder($player)
         ->add('email', TextType::class, array('label' => 'Email :'))
         ->add('password', PasswordType::class, array('label' => 'Mot de passe :'))
-        ->add('registration', SubmitType::class, array('label' => 'Inscription'))
+        ->add('login', SubmitType::class, array('label' => 'Login'))
         ->getForm();
 
         $form->handleRequest($request);
+
+
 
         if($form->isSubmitted() && $form->isValid())
         {
             $password = $form['password']->getData();
             $email = $form['email']->getData();
             $encoded_pass = sha1($form['password']->getData());
-
+            $date = date_create();
+            /*
+                Recherche dans la base de données si les différents éléments entrés
+                sont présents afin de connecter la personne
+            */
             $player = $this->getDoctrine()
                          ->getRepository('AppBundle:Player')
-                         ->findByEmail($email);
+                         ->findOneByEmail($email);
             $pass_check = $this->getDoctrine()
                          ->getRepository('AppBundle:Player')
                          ->findByPassword($encoded_pass);
-            console.log($player);
+
             if(!$player)
             {
-                throw $this->createNotFoundException('Le mot de passe ou l\'email est faux'.$player);
+                return $this->redirectToRoute('registration');
             }
             else
             {
-
-                return $this->redirectToRoute('login',  array('player' => $player));
+                $pseudo = $this->getDoctrine()
+                    ->getRepository('AppBundle:Player')
+                    ->findOneByEmail($email)->getPseudo();
+                /* Met à jour la date de connection */
+                $player->setDateLog($date);
+                /* Entre les différents élements dans la base */
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($player); // prépare l'insertion dans la BD
+                $em->flush(); // insère dans la BD
+                return $this->redirectToRoute('accueil',  array('pseudo' => $pseudo));
             }
-
-           
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($player); // prépare l'insertion dans la BD
-            $em->flush(); // insère dans la BD
         }
-
 
         return $this->render('Sko/menu.html.twig', array('form' => $form->createView()));
     }
 
     /**
-     * @Route("/registration", name="registration_page")
+     * @Route("/registration", name="registration")
      */
     public function registrationAction(Request $request)
     {
 
         $player = new Player;
-        
+
         $form = $this->createFormBuilder($player)
         ->add('pseudo', TextType::class, array('label' => 'Pseudo :'))
         ->add('email', TextType::class, array('label' => 'Email :'))
         ->add('password', PasswordType::class, array('label' => 'Mot de passe :'))
         ->add('registration', SubmitType::class, array('label' => 'Inscription'))
         ->getForm();
-        
+
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid())
@@ -86,19 +93,23 @@ class SkOController extends Controller
             $pseudo = $form['pseudo']->getData();
             $email = $form['email']->getData();
             $encoded_pass = sha1($form['password']->getData());
+            $date = date_create();
             $player->setPseudo($pseudo);
             $player->setEmail($email);
             $player->setPassword($encoded_pass);
+            $player->setDateLog($date);
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($player); // prépare l'insertion dans la BD
             $em->flush(); // insère dans la BD
+            return $this->render('Sko/accueil.html.twig', array('pseudo' => $pseudo));
         }
-
-
-
         return $this->render('Sko/registration.html.twig', array('form' => $form->createView()));
     }
+
+
+
+
 
     /**
      * @Route("/login", name="login")
@@ -107,8 +118,16 @@ class SkOController extends Controller
     {
         $em = $this->getDoctrine()->getEntityManager();
 
-        return $this->render('Sko/login.html.twig', array(
-
-            ));
+        return $this->render('Sko/login.html.twig', array());
     }
+
+    /**
+     * @Route("/accueil", name="accueil")
+     */
+    public function accueilAction(Request $request)
+    {
+        $player = $request->query->get('pseudo');
+        return $this->render('Sko/accueil.html.twig', array('pseudo' => $player));
+    }
+
 }
---
+++ src/AppBundle/Entity/Characters.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Characters
+ *
+ * @ORM\Table(name="characters")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CharactersRepository")
+ */
+class Characters
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     * @ORM\ManyToOne(targetEntity="Player", inversedBy="characters")
+     * @ORM\JoinColumn(name="id_player", referencedColumnName="id")
+     */
+    private $player;
+
+    /**
+     * @var int
+     * @ORM\Column(name="id_player", type="integer")
+     */
+    private $idPlayer;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pseudo", type="string", length=255, unique=true)
+     */
+    private $pseudo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="buildings", type="array")
+     */
+    private $buildings;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="units", type="array")
+     */
+    private $units;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="ressources", type="array")
+     */
+    private $ressources;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idPlayer
+     *
+     * @param integer $idPlayer
+     *
+     * @return Characters
+     */
+    public function setIdPlayer($idPlayer)
+    {
+        $this->idPlayer = $idPlayer;
+
+        return $this;
+    }
+
+    /**
+     * Get idPlayer
+     *
+     * @return int
+     */
+    public function getIdPlayer()
+    {
+        return $this->idPlayer;
+    }
+
+    /**
+     * Set pseudo
+     *
+     * @param string $pseudo
+     *
+     * @return Characters
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    /**
+     * Get pseudo
+     *
+     * @return string
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Characters
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set buildings
+     *
+     * @param array $buildings
+     *
+     * @return Characters
+     */
+    public function setBuildings($buildings)
+    {
+        $this->buildings = $buildings;
+
+        return $this;
+    }
+
+    /**
+     * Get buildings
+     *
+     * @return array
+     */
+    public function getBuildings()
+    {
+        return $this->buildings;
+    }
+
+    /**
+     * Set units
+     *
+     * @param array $units
+     *
+     * @return Characters
+     */
+    public function setUnits($units)
+    {
+        $this->units = $units;
+
+        return $this;
+    }
+
+    /**
+     * Get units
+     *
+     * @return array
+     */
+    public function getUnits()
+    {
+        return $this->units;
+    }
+
+    /**
+     * Set ressources
+     *
+     * @param array $ressources
+     *
+     * @return Characters
+     */
+    public function setRessources($ressources)
+    {
+        $this->ressources = $ressources;
+
+        return $this;
+    }
+
+    /**
+     * Get ressources
+     *
+     * @return array
+     */
+    public function getRessources()
+    {
+        return $this->ressources;
+    }
+
+    /**
+     * Set player
+     *
+     * @param \AppBundle\Entity\Player $player
+     *
+     * @return Characters
+     */
+    public function setPlayer(\AppBundle\Entity\Player $player = null)
+    {
+        $this->player = $player;
+
+        return $this;
+    }
+
+    /**
+     * Get player
+     *
+     * @return \AppBundle\Entity\Player
+     */
+    public function getPlayer()
+    {
+        return $this->player;
+    }
+}
