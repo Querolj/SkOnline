c5d76115f917a4f35087c9c622175c0fb53bd002
Nquere <nquere.8@gmail.com>
2017-05-07 17:35:31 +0100

    Mise à jour des derniers debug

--- src/AppBundle/Controller/SkOController.php
+++ src/AppBundle/Controller/SkOController.php
@@ -163,12 +163,13 @@ class SkOController extends Controller
      */
     public function accueilAction(Request $request)
     {
-        $pseudo = $request->query->get('pseudo');
-        $player = $this->getDoctrine()
-            ->getRepository('AppBundle:Player')
-            ->findOneByPseudo($pseudo);
+        $player = $this->container->get("security.token_storage")->getToken()->getUser();
+        // $pseudo = $request->query->get('pseudo');
+        // $player = $this->getDoctrine()
+        //     ->getRepository('AppBundle:Player')
+        //     ->findOneByPseudo($pseudo);
         $persos = $player->getCharacters();
-        return $this->render('Sko/accueil.html.twig', array('pseudo' => $pseudo, 'persos' => $persos));
+        return $this->render('Sko/accueil.html.twig', array('pseudo' => $player->getPseudo(), 'persos' => $persos));
     }
 
 
@@ -447,11 +448,12 @@ class SkOController extends Controller
     public function uniteAction(Request $request)
     {
         $pseudo = $request->query->get('perso');
-        $pseudoPlayer = $request->query->get('pseudo');
+        //$pseudoPlayer = $request->query->get('pseudo');
         $perso = $this->getDoctrine()
             ->getRepository('AppBundle:Characters')
             ->findOneByPseudo($pseudo);
-        return $this->render('Sko/unite.html.twig', array('pseudo' => $pseudoPlayer,'perso' => $perso));
+        $player = $this->container->get("security.token_storage")->getToken()->getUser();
+        return $this->render('Sko/unite.html.twig', array('pseudo' => $player->getPseudo(),'perso' => $perso));
     }
 
     /**
@@ -470,14 +472,14 @@ class SkOController extends Controller
 
         if($form->isSubmitted() && $form->isValid())
         {
-            $pseudo = $request->query->get('pseudo');
-            $player = $this->getDoctrine()
-                ->getRepository('AppBundle:Player')
-                ->findOneByPseudo($pseudo);
+            $player = $this->container->get("security.token_storage")->getToken()->getUser();
+            // $pseudo = $request->query->get('pseudo');
+            // $player = $this->getDoctrine()
+            //     ->getRepository('AppBundle:Player')
+            //     ->findOneByPseudo($pseudo);
             $idPlayer = $player->getId();
             $name = $form['pseudo']->getData();
             $image = $form['image']->getData();
-            $village = "Village 1";
 
             $file = $character->getImage();
            $fileName = uniqid().'.'.$file->guessExtension();
@@ -497,7 +499,6 @@ class SkOController extends Controller
            $units = new units;
            $ressources = new ressources;
 
-            $building->setPseudo($village);
             $building->setPerso($character);
             $em = $this->getDoctrine()->getManager();
             $em->persist($building); // prépare l'insertion dans la BD
@@ -513,7 +514,7 @@ class SkOController extends Controller
             $em->persist($ressources); // prépare l'insertion dans la BD
             $em->flush();
 
-            return $this->render('Sko/accueil.html.twig', array('pseudo' => $pseudo, 'persos' => $player->getCharacters()));
+            return $this->render('Sko/accueil.html.twig', array('pseudo' => $player->getPseudo(), 'persos' => $player->getCharacters()));
         }
         return $this->render('Sko/createPerso.html.twig', array('form' => $form->createView()));
     }
@@ -522,9 +523,7 @@ class SkOController extends Controller
      */
     public function uniteLogout(Request $request)
     {
-        return $this->render('Sko/menu.html.twig', array(
-
-            ));
+        return $this->render('Sko/menu.html.twig', array());
     }
     /**
      * @Route("/save", name="save")
